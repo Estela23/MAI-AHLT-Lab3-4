92654fbfff5bc32bcc9e0c7e8b1b04c7e6214dce
FernandoVN98 <38290736+FernandoVN98@users.noreply.github.com>
2021-04-29 20:52:41 +0200

    3 primeras features, REVISAR LAS REGLAS

--- Lab4/classifierMEGAM.py
+++ Lab4/classifierMEGAM.py
@@ -2,30 +2,19 @@
 import pickle
 import sys
 
-from sklearn.preprocessing import OneHotEncoder
-
 model_to_use = sys.argv[1]
 file_to_classify = sys.argv[2]
 file_to_write = sys.argv[4]
 mymodel = pickle.load(open(model_to_use, 'rb'))
 file = open(file_to_classify, "r")
 data_init = file.readlines()
-data=[]
-SID=[]
 for line in data_init:
         # split  line  into  elements
         fields = line.strip('\n').split("\t")
         # first 4 elements  are sid ,e1 ,e2 , and  groundtruth (ignored  since we are  classifying)
         (sid ,e1,e2,gt) = fields [0:4]
-        SID.append(fields[0:4])
         # Rest of  elements  are  features , passed  to theclassifier  of  choice  to get a prediction
-        data.append(fields[4:])
-encoder = OneHotEncoder()
-encoded_df = encoder.fit_transform(data)
-X_sentences = encoded_df
-predictions = mymodel.predict(X_sentences)
-# if the  classifier  predicted a DDI , output  itin the  right  format
-with open(file_to_write, 'w') as output:
-    for prediction, sid in zip(predictions, SID):
-        if prediction != "null":
-            print(sid[0] ,sid[1],sid[2],prediction ,sep="|", file=output)
+        prediction = mymodel.predict(fields[4])
+        # if the  classifier  predicted a DDI , output  itin the  right  format
+        if prediction  != "null" :
+            print(sid ,e1,e2,prediction ,sep="|")
--- Lab4/extract_features.py
+++ Lab4/extract_features.py
@@ -15,16 +15,91 @@ def extract_features(tree , entities , e1, e2):
      [’lb1 = Caution ’, ’lb1 = be’, ’lb1 = exercise ’, ’lb1 = combine ’, ’lib = or ’, ’lib
      = salicylic ’, ’lib = acid ’, ’lib = with ’, ’LCSpos=VBG ’, ’LCSlema=combine’
      , ’path=dobj / combine\nmod\compound ’ ’entity_in_between ’]'''
-    if rule_verb_middle(tree, entities, e1, e2) is not None:
-        if rule_verb_middle(tree, entities, e1, e2)=='verb_effect_middle':
-            return ['1']
-        elif rule_verb_middle(tree, entities, e1, e2)=='verb_mechanism_middle':
-            return ['2']
-        else:
-            return ['3']
-    else:
-        return ['0']
+    features=[]
+    rule_v_m = rule_verb_middle(tree, entities, e1, e2)
+    if rule_v_m is not None:
+        features.append(rule_v_m)
+    paths = path_verbs(tree, entities, e1, e2)
+    if paths is not None:
+        features.append("path1="+paths)
+    paths = path_entities(tree, entities, e1, e2)
+    if paths is not None:
+        features.append("path2="+paths)
+    return features
+def path_entities(analysis, entities, e1, e2):
+    first_entity=""
+    second_entity=""
+    lengths = {key: len(value) for key, value in entities.items()}
+    if e1 in entities and e2 in entities:
+        start_e1 = int(entities[e1][0])
+        start_e2 = int(entities[e2][0])
+        flag_1 = False
+        flag_2 = False
+        if lengths[e1] == 2 and lengths[e2] == 2:
+            end_e1 = int(entities[e1][1])
+            end_e2 = int(entities[e2][1])
+            for i in range(1, len(analysis.nodes) + 1):
+                if "start" in analysis.nodes[i] and "end" in analysis.nodes[i]:
+                    if analysis.nodes[i]["start"] == start_e1 and analysis.nodes[i]["end"] ==end_e1:
+                        first_entity=analysis.nodes[i]
+                    elif analysis.nodes[i]["start"] == start_e2 and analysis.nodes[i]["end"] ==end_e2:
+                        second_entity=analysis.nodes[i]
+            if first_entity!="" and second_entity!="":
+                aux1=first_entity
+                path = ""
+                while aux1['head'] is not None and aux1["start"] != start_e2 and aux1["end"] != end_e2:
+                    aux1 = analysis.nodes[aux1["head"]]
+                    path = path + "/" + str(aux1["rel"])
+                aux2=second_entity
+                path2 = ""
+                while aux2['head'] is not None and aux2["start"] != start_e1 and aux2["end"] != end_e1:
+                    aux2 = analysis.nodes[aux2["head"]]
+                    path2 = path2 + "/" + str(aux2["rel"])
+                if "start" in aux1 and "end" in aux1:
+                    if aux1["start"] == start_e2 and aux1["end"] == end_e2:
+                        flag_1 = True
+                if "start" in aux2 and "end" in aux2:
+                    if aux2["start"] == start_e1 and aux2["end"] == end_e1:
+                        flag_2 = True
+                if flag_1:
+                    return path
+                elif flag_2:
+                    return path2
 
+    return None
+def path_verbs(analysis, entities, e1, e2):
+    lengths = {key: len(value) for key, value in entities.items()}
+    if e1 in entities and e2 in entities:
+        start_e1 = int(entities[e1][0])
+        start_e2 = int(entities[e2][0])
+        flag_1=False
+        flag_2=False
+        if lengths[e1] == 2 and lengths[e2] == 2:
+            end_e1 = int(entities[e1][1])
+            end_e2 = int(entities[e2][1])
+            for i in range(1, len(analysis.nodes) + 1):
+                if "tag" in analysis.nodes[i] and "start" in analysis.nodes[i] and "end" in analysis.nodes[i]:
+                    if analysis.nodes[i]["tag"] == "VB" or analysis.nodes[i]["tag"] == "VBD":
+                        aux1=analysis.nodes[analysis.nodes[i]["head"]]
+                        aux2 = analysis.nodes[analysis.nodes[i]["head"]]
+                        path = str(aux1["rel"])
+                        path2=str(aux2["rel"])
+                        while aux1['head'] is not None and aux1["start"] != start_e1 and aux1["end"] != end_e1:
+                            aux1=analysis.nodes[aux1["head"]]
+                            path=path+"/"+str(aux1["rel"])
+
+                        while aux2['head'] is not None and aux2["start"] != start_e2 and aux2["end"] != end_e2:
+                            aux2=analysis.nodes[aux2["head"]]
+                            path2=path2+"/"+str(aux2["rel"])
+                        if "start" in aux1 and "end" in aux1 and "start" in aux2 and "end" in aux2:
+                            if aux1["start"] == start_e1 and aux1["end"] == end_e1:
+                                flag_1=True
+                            if aux2["start"] == start_e2 and aux2["end"] == end_e2:
+                                flag_2=True
+                            lemma="<"+analysis.nodes[i]['lemma']+">"#TODO: REVISAR EL CODIGO PUEDE QUE ESTE MAL OJO
+                            #paths.append(["<"+analysis.nodes[i]['lemma']+">"])
+                            return path+lemma+path2
+    return None
 def rule_verb_middle(analysis, entities, e1, e2):
     effect = ['administer', 'potentiate', 'prevent', 'react', 'produce', 'attenuate', 'treat', 'alter', 'augment',
               'influence', 'prevent', 'antagonize', 'augment', 'block', 'cause']
